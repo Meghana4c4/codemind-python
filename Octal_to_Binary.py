83749135b038bcb42244721d05a008106e75afe4
Meghana4c4 <84376992+Meghana4c4@users.noreply.github.com>
2022-08-15 15:14:27 +0530

    for _ in range(int(input())):     q=int(input())     s,i=0,0     while q:         n=q%10         s+=n*(8**i)         q=q//10         i+=1     q=s     l=[]     while q:         r=q%2         l.append(r)         q=q//2     l.reverse()     print(*l, sep='')

--- Octal_to_Binary.py
+++ Octal_to_Binary.py
@@ -1,16 +1,16 @@
-for _ in range(int(input())):
-    q=int(input())
-    s,i=0,0
-    while q:
-        n=q%10
-        s+=n*(8**i)
-        q=q//10
-        i+=1
-    q=s
-    l=[]
-    while q:
-        r=q%2
-        l.append(r)
-        q=q//2
-    l.reverse()
+for _ in range(int(input())): 
+    q=int(input()) 
+    s,i=0,0 
+    while q: 
+        n=q%10 
+        s+=n*(8**i) 
+        q=q//10 
+        i+=1 
+    q=s 
+    l=[] 
+    while q: 
+        r=q%2 
+        l.append(r) 
+        q=q//2 
+    l.reverse() 
     print(*l, sep='')
