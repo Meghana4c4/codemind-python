3db899ce6d1a4a00ee2061675979c0b6be9e603f
Meghana4c4 <84376992+Meghana4c4@users.noreply.github.com>
2022-09-16 13:12:59 +0530

    n=input() n=n.split() c=0 vow='AEIOUaeiou'
    for i in n:
        a,b=0,len(i)-1
        while (a<b and a!=b):
            if (i[a] in vow and i[b] not in vow):
                c+=1
            elif (i[a] not in vow and i[b] in vow):
                c+=1
            a+=1
            b-=1
    print(c)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -2,15 +2,14 @@ n=input()
 n=n.split()
 c=0
 vow='AEIOUaeiou'
+
 for i in n:
     a,b=0,len(i)-1
-    while a<b and a!=b:
-        if i[a] in vow and i[b] not in vow:
+    while (a<b and a!=b):
+        if (i[a] in vow and i[b] not in vow):
             c+=1
-        elif i[a] not in vow and i[b] in vow:
+        elif (i[a] not in vow and i[b] in vow):
             c+=1
         a+=1
         b-=1
 print(c)
-    
-    
