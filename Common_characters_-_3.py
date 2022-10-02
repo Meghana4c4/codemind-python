47a31c160d5c0cd70ff489f43779e64238e2a05d
Meghana4c4 <84376992+Meghana4c4@users.noreply.github.com>
2022-10-02 15:50:52 +0530

    n=input().lower().split() c=[] for i in range(97,123):     c=0     for d in n:         if chr(i) in d:             c+=1     if c==len(n):         print(chr(i))         break else:     print('-1')

---
+++ Common_characters_-_3.py
@@ -0,0 +1,12 @@
+n=input().lower().split()
+c=[]
+for i in range(97,123):
+    c=0
+    for d in n:
+        if chr(i) in d:
+            c+=1
+    if c==len(n):
+        print(chr(i))
+        break
+else:
+    print('-1')
